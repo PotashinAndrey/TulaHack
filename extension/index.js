42036eb4d87313973c83f8460fe9202f9517747b
andrei potashin <a.potashin@pnlfin.tech>
2022-04-23 17:07:30 +0500

    commit

--- extension/index.js
+++ extension/index.js
@@ -1,4 +1,56 @@
-const URL = "http://lovalhost:7443";
+const iframeSrc = "https://localhost:7443";
+
+const openAuc = () => {
+    const modal = document.createElement("div");
+    const wrapper = document.createElement("div");
+    const closeBtn = document.createElement("div");
+
+    wrapper.id = "modal-auction"
+
+    wrapper.style.position = "fixed";
+    wrapper.style.width = "100%";
+    wrapper.style.height = "100%";
+    wrapper.style.zIndex = "100";
+    wrapper.style.background = "rgba(0, 0, 0, 0.5)"
+
+    modal.style.width = "800px";
+    modal.style.height = "700px";
+    modal.style.background = "white";
+    modal.style.borderRadius = "10px";
+    modal.style.marginLeft = "50%";
+    modal.style.marginTop = "35%";
+    modal.style.transform = "translate(-50%, -50%)";
+    modal.style.position = "relative";
+
+    closeBtn.style.position = "absolute";
+    closeBtn.style.top = "0";
+    closeBtn.style.right = "-90px";
+    closeBtn.style.width = "30px";
+    closeBtn.style.height = "30px";
+    closeBtn.style.background = "white";
+    closeBtn.style.borderRadius = "100%";
+    closeBtn.style.paddingTop = "6px";
+    closeBtn.style.textAlign = "center";
+    closeBtn.style.boxSizing = "border-box";
+    closeBtn.style.cursor = "pointer";
+    closeBtn.innerHTML = "×";
+
+
+    closeBtn.addEventListener("click", () => {
+        document.getElementById("modal-auction").remove();
+    });
+
+    const iframe = document.createElement("iframe");
+    iframe.src = iframeSrc;
+    iframe.style.width = "100%";
+    iframe.style.height = "100%";
+
+    modal.appendChild(iframe);
+    modal.appendChild(closeBtn);
+
+    wrapper.appendChild(modal);
+    document.body.appendChild(wrapper);
+};
 
 const header = document.getElementsByClassName("index-inner-iPEdy")[0];
 const newDiv = document.createElement('div');
@@ -9,33 +61,38 @@ const newA = document.createElement("a");
 newA.classList = "index-add-button-RRqyw button-button-CmK9a button-size-s-r9SeD button-primary-x_x8w";
 newA.innerText = "Создать аукцион";
 
-newA.addEventListener("click", () => {
-    const modal = document.createElement("div");
-    const wrapper = document.createElement("div");
+newA.addEventListener("click", openAuc);
 
-    wrapper.id = "modal-auction"
+newDiv?.appendChild(newA);
+header?.appendChild(newDiv);
 
-    wrapper.style.position = "fixed";
-    wrapper.style.width= "100%";
-    wrapper.style.height = "100%";
-    wrapper.style.zIndex = "100";
-    wrapper.style.background = "rgba(0, 0, 0, 0.5)"
 
-    modal.style.width = "500px";
-    modal.style.height = "500px";
-    modal.style.background = "white";
-    modal.style.borderRadius = "10px";
-    modal.style.marginLeft = "50%";
-    modal.style.marginTop = "50%";
-    modal.style.transform = "translate(-50%, -50%)";
+setTimeout(() => {
+    const actions = document.getElementsByClassName("js-item-actions")[0];
+    // console.log(document.getElementsByClassName("js-item-actions"),actions)
 
-    modal.addEventListener("click", () => {
-        document.getElementById("modal-auction").remove();
-    });
+    if (actions) {
+        // console.log(actions);
+        const itemLine = document.createElement("div");
+        itemLine.classList = "item-actions-line";
 
-    wrapper.appendChild(modal);
-    document.body.appendChild(wrapper);
-});
+        const span = document.createElement("span");
+        span.classList = "js-messenger-button";
 
-newDiv.appendChild(newA);
-header.appendChild(newDiv);
+        const button = document.createElement("button");
+        button.classList = "button-button-2Fo5k button-size-l-3LVJf width-width-12-2VZLz"
+        button.innerText = "Участвовать в аукционе"
+        button.style.border = "none";
+        button.style.background = "#c722b6";
+        button.style.color = "white";
+        button.style.height = "66px";
+
+        button.onclick = openAuc;
+        
+        span.appendChild(button);
+        itemLine.appendChild(span);
+        actions.appendChild(itemLine);
+
+
+    }
+}, 1000);
