da14ce84be417f852ead6dab14ed9a56a3797e98
melogo5 <i.plyton@gmail.com>
2022-04-23 15:56:41 +0500

    страничка с аукционом

--- frontend/pages/CreateAuc.js
+++ frontend/pages/CreateAuc.js
@@ -1,4 +1,5 @@
 import Component, { html, css } from '../class/Component.js';
+import AppButton from '../components/app-button.js';
 import $ from '../class/DOM.js';
 import locator from '../script/locator.js';
 
@@ -18,6 +19,57 @@ const style = css`
     height: 100vh;
     font-family: var(--font);
   }
+  #upload-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 400px;
+    height: 400px;
+    outline: 2px dashed #5d5d5d;
+    outline-offset: -12px;
+    background-color: #e0f2f7;
+    font-family: 'Segoe UI';
+    color: #1f3c44;
+  }
+  
+  #upload-container img {
+    width: 40%;
+    margin-bottom: 20px;
+    user-select: none;
+  }
+  
+  #upload-container label {
+    font-weight: bold;
+  }
+  
+  #upload-container label:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+  
+  #upload-container div {
+    position: relative;
+    z-index: 10;
+  }
+  
+  #upload-container input[type=file] {
+    width: 0.1px;
+    height: 0.1px;
+    opacity: 0;
+    position: absolute;
+    z-index: -10;
+  }
+  
+  #upload-container label.focus {
+    outline: 1px solid #0078d7;
+    outline: -webkit-focus-ring-color auto 5px;
+  }
+  
+  #upload-container.dragover {
+    background-color: #fafafa;
+    outline-offset: -17px;
+  }
   slot {
     display: block;
     position: relative;
@@ -25,9 +77,6 @@ const style = css`
     overflow: auto;
     overscroll-behavior-y: contain;
     /* padding-bottom: 80px; */
-  }
-  app-bar {
-    text-align: center;
   }`;
 
 /** Раскладка {CreateAuc} @class @ui @component <create-auc />
@@ -38,7 +87,18 @@ export default class CreateAuc extends Component {
     <template>
       <style>${style}</style>
       <div id="root">
-        Привет
+        <h1>Название лота:</h1>
+        <input type="text" size="40">
+        <h1>Загрузите изображения товара:</h1>
+        <img id="upload-image" src="icons/upload.svg">
+        <div>
+          <input id="file-input" type="file" name="file" multiple>
+          <label for="file-input">Выберите файл</label>
+          <span>или перетащите его сюда</span>
+        </div>
+        <h1>Введите стартовую цену торговли:</h1>
+        <input type="text">
+        <app-button primary wide id="submitBtn">Создать</app-button>
         <slot></slot>
       </div>
     </template>`;
